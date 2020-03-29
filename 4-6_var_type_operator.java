9a8bd5c1d28ae92703616a8b7ea5e5b8f19a61d1
Johnson Huang 黃聖翔 <johnson.huang@gogoro.com>
2020-03-29 15:38:08 +0800

    2020-03-29T15 operator, comment

---
+++ 4-6_var_type_operator.java
@@ -0,0 +1,9 @@
+public class TestDemo{
+    // 4. Method
+    public static void main(String[] args){
+        int a = 3;
+        int b = 4;
+        System.out.println((a!=b)&&(b%a>0));
+        System.out.println(5%3); // % to find remainder
+    }
+}
---
+++ 4-7_var_type_comment.java
@@ -0,0 +1,17 @@
+
+public class TestDemo{
+    // 4. Method
+    public static void main(String[] args){
+        /**
+         * This is the first way to write a comment in Java. 
+         * (For multiple lines)
+         * 只要打 斜線＋兩顆星星，按Enter之後就會一直可以寫註解
+         * /
+        
+        /* This is the first way to write a comment in Java. 
+         * (For multiple lines) 
+         * /
+        
+        // This is the third way to write a comment in Java.
+    }
+}
