bcfee276519c86701b0cde98698d52bb0dca3716
Johnson Huang 黃聖翔 <johnson.huang@gogoro.com>
2020-03-31 00:09:18 +0800

    2020-03-31T00 string startsWith endsWith

---
+++ 5-7_String_startsWith_endsWith.java
@@ -0,0 +1,12 @@
+public class TestDemo{
+    // 4. Method
+    public static void main(String[] args){
+        String str = "abc";
+
+        // startsWith
+        System.out.println(str.startsWith("a")); // true
+
+        // endsWith
+        System.out.println(str.endsWith("c")); // true
+    }
+}
