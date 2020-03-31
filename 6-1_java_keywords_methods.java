6ca9cea459d029bfeb92ade2f5f112bb48792d10
Johnson Huang 黃聖翔 <johnson.huang@gogoro.com>
2020-03-31 18:59:43 +0800

    2020-03-31T18 reserved keywords and methods in Java

--- 6-1_java_keywords_methods.java
+++ 6-1_java_keywords_methods.java
@@ -1,6 +1,6 @@
 /**
 reserved keywords in Java :
-    - 存取權限控制的關鍵字(private, default, protected, public), fianl, new, static, import, package
+    - access modifiers 存取權限控制的關鍵字(private, default, protected, public), fianl, new, static, import, package
 
 methods in Java :
     - main, 宣告自定義的方法, override
