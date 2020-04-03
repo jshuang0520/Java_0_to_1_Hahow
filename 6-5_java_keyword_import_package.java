d356179db069f07d966918929f5b372f55aa9483
Johnson Huang 黃聖翔 <johnson.huang@gogoro.com>
2020-04-03 12:22:33 +0800

    2020-04-03T12 keyword: import, package

---
+++ 6-5_java_keyword_import_package.java
@@ -0,0 +1,32 @@
+/**
+
+keyword 
+
+- package :
+to manage classes, imagine it's like a folder
+
+- import :
+to import classes
+
+
+
+If there's a folder structure like this:
+java_0_to_1
+│
+├── Pokemon
+│   └── Pikachu.java
+│
+├── MySheets
+    ├── Sheet1
+    │   └── Pikachu.java
+    └──Sheet2
+       └── MulticolouredBedSheet.java
+    
+
+Write the "full path" of package you want to import :
+import java_0_to_1.Pokemon.Pikachu
+import java_0_to_1.MySheets.Sheet1.Pikachu
+
+Then you won't be confused by which Pikachu you're actually using down below.
+
+ */
