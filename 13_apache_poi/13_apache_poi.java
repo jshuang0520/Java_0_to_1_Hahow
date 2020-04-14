6f4a2ac0989802b74caa6e18b3bb8b1ee233e0af
Johnson Huang 黃聖翔 <johnson.huang@gogoro.com>
2020-04-14 14:15:11 +0800

    2020-04-14T14 java - Excel - Apache POI

---
+++ 13_apache_poi/13_apache_poi.java
@@ -0,0 +1,33 @@
+/**
+
+
+---------------------------------------------------
+-------------------------
+Apache POI - the Java API for Microsoft Documents
+-------------------------
+Home     - https://poi.apache.org/
+Download - https://poi.apache.org/download.html
+JavaDocs - https://poi.apache.org/apidocs/index.html
+
+---
+(We now use MacOS, Eclipse as example)
+---
+To use SDK from the third party, you need to: 
+
+1. Download a "jar" file
+    (1) Download - https://poi.apache.org/download.html
+    (2) Choose "the latest stable release is Apache POI ___"
+    (3) Find "Binary Distribution" -> "poi-bin-...-.tar.gz"
+2. Import it to your IDE (ex. Eclipse)
+    (1) Drag the "jar" file to your "lib" folder in your "current project folder"
+    (2) Project -> Properties
+    (3) Java Build Path -> Libraries -> Add JARs
+    (DO NOT click "add jars" for your .java files)
+
+3. Read its API document to use its methods
+
+
+
+---------------------------------------------------
+
+*/
---
+++ 13_apache_poi/ExcelUtil.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtil{
+
+    public static void main(String[] args){
+		System.out.println("good");
+	}
+}
