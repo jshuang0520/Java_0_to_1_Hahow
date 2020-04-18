89124feff0c774bdeb66a11a926b992bf2991309
Johnson Huang 黃聖翔 <johnson.huang@gogoro.com>
2020-04-18 19:33:18 +0800

    2020-04-18T19 intro of tech application

---
+++ 15_code_and_tech/15_code_and_tech.java
@@ -0,0 +1,18 @@
+/**
+
+
+Learning java code:
+1. Engineer
+2. AI
+    GCP (Google Cloud Platform)    https://cloud.google.com/products/
+    - AI and Machine Learning
+        - cloud vision api    https://cloud.google.com/vision/docs
+3. AR (Augmented Reality)
+    - NyARToolkit    http://nyatla.jp/nyartoolkit/wp/?page_id=14
+    - wikitude       https://www.wikitude.com/products/wikitude-sdk/
+4. API / SDK
+    - Facebook API, SDK    https://developers.facebook.com/docs/apis-and-sdks
+                           https://developers.facebook.com/docs/apis-and-sdks?locale=en_US
+    - AWS Lambda (serverless environment)    https://aws.amazon.com/tw/lambda/details/
+
+*/
