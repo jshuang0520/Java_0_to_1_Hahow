c85621299f9a825708f6d09de94c525dc3704b04
Johnson Huang 黃聖翔 <johnson.huang@gogoro.com>
2020-04-18 17:15:10 +0800

    2020-04-18T15 multithread - synchronized: get 'lock flag' to be unblocked and back to RUNNABLE , wait: after enough  waiting then have qualification to get the 'lock flag'

--- 14-5_multithread_synchronized_wait_notify/14-5_multithread_synchronized_wait_notify.java
+++ 14-5_multithread_synchronized_wait_notify/14-5_multithread_synchronized_wait_notify.java
@@ -1,6 +1,50 @@
 /**
 
 
+---------------------------------------------------
+-------------------------
+同步 synchronized
+-------------------------
+多執行緒的缺點：
+1. 多執行緒在使用同一個資料（「多個執行緒共用同一個物件」）時，會出現資料不一致的情況
+    ex. 在多執行緒下，有人塞入資料有人讀取，萬一B執行緒本來要讀取原本的資料，卻因為A執行緒比他早一步塞入資料（更新掉原有的）才被B執行緒取得，就取錯了
+2. 因為多執行緒的執行時間不一定，所以問題不一定會發生（但仍然有機會發生）
+
+解決方法：
+1. keyword "synchronized"
+    (1) to ensure there's ONLY ONE thread using this object
+    (2) 同步化區塊 synchronized(this){
+                     // do something in this synchronized section...
+                  }
+    (3) 物件的鎖定(lock)，執行緒要取得lock才能執行synchronized裡面的內容（每個物件的身上都有一個 lock flag）
+
+
+class User{
+    // 2. Instance variable
+    private String name;
+
+    // 4. Method
+    // ---------- keyword "synchronized" : to solve things bothered by multi-thread issue ----------
+    public synchronized setName(String name){
+        this.name = name;
+    }
+
+    // 4. Method
+    public getName{
+        return name;
+    }
+}
+
+ThreadA threadA = new ThreadA(user);
+ThreadB threadB = new ThreadB(user);
+
+
+---------------------------------------------------
+
+
+
+
+
 
 new Thread                                                         Dead
 (Constructor1. Thread())                                             ^
@@ -29,12 +73,16 @@ new Thread                                                         Dead
                                                     |                               |
                                                     |                               |
                        ^                            |                               |
-                       |                            |                               |
-                       |                            |                               |
-                       |                            |                               |
+Unblocked              |                            |                               |
+the thread gets the    |                            |                               |
+"lock flag" of the object                           |                               |
+is unblocked           |                            | synchronized                  | wait
                        |                            |                               |
                        |                            |                               |
                        |       (LOCKED POOL)        v                               |
+everyone here(lock pool)                                                            |
+need to rush for the "unique" lock flag for that synchronized object,               |
+then can go next stage(RUNNING) if thread_scheduler calls                           |
                        <-------- BLOCKED 1 <---------                               |
                     Unblocked          ^      Blocked_event                         |
                                        |      ex. synchronized                      |
@@ -43,6 +91,7 @@ new Thread                                                         Dead
                                        |                                            |
                                        |                                            |
                                        |                         (wait POOL)        |
+                                            you're NOT qualified to get "lock" flag in "wait pool"
                                         <------------------------ BLOCKED 2 <-------- 
                                     Unblocked                               Blocked_event
                                     ex. notify()                            ex. wait()
