8d9823c18345f8c5c7f59fc019547c21d19436aa
Johnson Huang 黃聖翔 <johnson.huang@gogoro.com>
2020-04-22 09:52:38 +0800

    2020-04-22T09 JSP implementation

--- 17-3_web_container/17-5_jsp_implementation.java
+++ 17-3_web_container/17-5_jsp_implementation.java
@@ -1,12 +1,5 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
+// If you want to write Java code in JSP, use this <% ...write some java code here... %>
+
 <%
 
 /*
@@ -39,6 +32,3 @@ out.println("Hello " + name);
 
 out.println("You can write Java code in JSP as long as using notation <% and its closing tag!\n");
 %>
-
-</body>
-</html>
